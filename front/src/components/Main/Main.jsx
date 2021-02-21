370a036482e0d27219405fcf8f17389dda72e116
Roman Yarovoy <roman@booma.co.il>
2021-02-21 12:15:20 +0200

    fix

---
+++ front/src/components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+
+const Main = () => {
+  
+  return (
+    <Wrapper>
+    </Wrapper>
+  )
+}
+
+export default Main
+
+const Wrapper = styled.div`
+`
--- src/components/Credential.js
+++
@@ -1,7 +0,0 @@
-const Credential = () => {
-  return {
-    ClientId: '207dfa1996ae47b5b145438fd7dc31c7',
-    ClientSecret: 'ee0ecd5b95374756ba121f8aa1205975',
-  };
-};
-export default Credential;
